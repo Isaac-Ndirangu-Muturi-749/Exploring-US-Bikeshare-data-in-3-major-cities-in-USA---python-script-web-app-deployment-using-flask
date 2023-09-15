8ff04ff7b92fead40d006fb8b7885ee0b8399db7
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-09-15 15:49:51 +0300

    Update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -31,44 +31,125 @@ def load_data(city, month, day):
         print("File not found for the selected city. Please check the file name.")
         return None
 
+from datetime import datetime
+
 def calculate_statistics(df):
-    # Calculate the average trip duration in seconds
-    avg_trip_duration_seconds = df['Trip Duration'].mean()
-    # Convert the average trip duration from seconds to minutes
-    avg_trip_duration_minutes = avg_trip_duration_seconds / 60
-    # Find the most popular starting station among users
-    most_popular_start_station = df['Start Station'].mode()[0]
-    # Find the most popular ending station among users
-    most_popular_end_station = df['End Station'].mode()[0]
-    # Calculate the total travel time in seconds
-    total_travel_time_seconds = df['Trip Duration'].sum()
-    # Convert the total travel time from seconds to minutes
-    total_travel_time_minutes = total_travel_time_seconds / 60
-    # Check if 'Birth Year' column exists in the DataFrame
-    if 'Birth Year' in df.columns:
-        # Get the current year
-        current_year = datetime.now().year
-        # Calculate the age of users based on their birth year
-        df['Age'] = current_year - df['Birth Year']
-        # Calculate the median age of users
-        median_age = df['Age'].median()
-        # Calculate the average age of users
-        average_age = df['Age'].mean()
+    if df.empty:
+        # Return an empty dictionary if the DataFrame is empty
+        return {}
+
+    results = {
+        "popular times of travel": {
+            "most common month": None,
+            "most popular day of the week for bike rentals": None,
+            "most common hour of day": None,
+        },
+        "popular stations and trip": {
+            "most popular start station": None,
+            "most popular end station": None,
+            "most common trip from start to end": None,
+            "number of unique starting stations": None,
+        },
+        "trip duration": {
+            "average trip duration (in minutes)": None,
+            "total travel time (in minutes)": None,
+            "longest trip duration recorded (in minutes)": None,
+        },
+        "user info": {
+            "counts of each user type": None,
+            "counts of each gender": None,
+            "earliest year of birth": None,
+            "most recent year of birth": None,
+            "most common year of birth": None,
+            "median age of users": None,
+            "average age of users": None,
+        }
+    }
+    
+    # Most common month
+    most_common_month = df['month'].mode()[0]
+    results["popular times of travel"]["most common month"] = most_common_month
+    
+    # Most common hour of the day
+    df['hour'] = df['Start Time'].dt.hour
+    most_common_hour = df['hour'].mode()[0]
+    results["popular times of travel"]["most common hour of day"] = most_common_hour
+    
     # Find the most popular day of the week for bike rentals
     most_popular_day = df['day_of_week'].mode()[0]
-    # Find the longest trip duration in seconds
-    longest_trip_duration_seconds = df['Trip Duration'].max()
-    # Convert the longest trip duration from seconds to minutes
-    longest_trip_duration_minutes = longest_trip_duration_seconds / 60
+    results["popular times of travel"]["most popular day of the week for bike rentals"] = most_popular_day
+    
+    # Find the most popular starting station among users
+    most_popular_start_station = df['Start Station'].mode()[0]
+    results["popular stations and trip"]["most popular start station"] = most_popular_start_station
+    
+    # Find the most popular ending station among users
+    most_popular_end_station = df['End Station'].mode()[0]
+    results["popular stations and trip"]["most popular end station"] = most_popular_end_station
+    
+    # Find the most common trip from start to end
+    most_common_trip = df['Start Station'] + " to " + df['End Station']
+    most_common_trip = most_common_trip.mode()[0]
+    results["popular stations and trip"]["most common trip from start to end"] = most_common_trip
+    
     # Count the number of unique starting stations
     unique_starting_stations = df['Start Station'].nunique()
-
-    return {"\nAverage trip duration (in minutes)": round(avg_trip_duration_minutes, 2), "\nMost popular start station": most_popular_start_station, "\nMost popular end station": most_popular_end_station, "\nTotal travel time (in minutes)": round(total_travel_time_minutes, 2), "\nMedian age of users": int(median_age) if 'Birth Year' in df.columns else None, "\nAverage age of users": round(average_age, 2) if 'Birth Year' in df.columns else None, "\nMost popular day of the week for bike rentals": most_popular_day, "\nLongest trip duration recorded (in minutes)": round(longest_trip_duration_minutes, 2), "\nNumber of unique starting stations": unique_starting_stations}
+    results["popular stations and trip"]["number of unique starting stations"] = unique_starting_stations
+    
+    # Calculate the average trip duration in minutes
+    avg_trip_duration_minutes = df['Trip Duration'].mean() / 60
+    results["trip duration"]["average trip duration (in minutes)"] = round(avg_trip_duration_minutes, 2)
+    
+    # Calculate the total travel time in minutes
+    total_travel_time_minutes = df['Trip Duration'].sum() / 60
+    results["trip duration"]["total travel time (in minutes)"] = round(total_travel_time_minutes, 2)
+    
+    # Find the longest trip duration in minutes
+    longest_trip_duration_minutes = df['Trip Duration'].max() / 60
+    results["trip duration"]["longest trip duration recorded (in minutes)"] = round(longest_trip_duration_minutes, 2)
+    
+    # Check if 'User Type' column exists in the DataFrame
+    if 'User Type' in df.columns:
+        # Counts of each user type
+        user_type_counts = df['User Type'].value_counts().to_dict()
+        results["user info"]["counts of each user type"] = user_type_counts
+    
+    # Check if 'Gender' column exists in the DataFrame
+    if 'Gender' in df.columns:
+        # Counts of each gender
+        gender_counts = df['Gender'].value_counts().to_dict()
+        results["user info"]["counts of each gender"] = gender_counts
+    
+    # Check if 'Birth Year' column exists in the DataFrame
+    if 'Birth Year' in df.columns:
+        # Get the earliest year of birth
+        earliest_birth_year = df['Birth Year'].min()
+        results["user info"]["earliest year of birth"] = int(earliest_birth_year)
+        
+        # Get the most recent year of birth
+        most_recent_birth_year = df['Birth Year'].max()
+        results["user info"]["most recent year of birth"] = int(most_recent_birth_year)
+        
+        # Get the most common year of birth
+        most_common_birth_year = df['Birth Year'].mode()[0]
+        results["user info"]["most common year of birth"] = int(most_common_birth_year)
+        
+        # Calculate the median age of users
+        current_year = datetime.now().year
+        df['Age'] = current_year - df['Birth Year']
+        median_age = df['Age'].median()
+        results["user info"]["median age of users"] = int(median_age)
+        
+        # Calculate the average age of users
+        average_age = df['Age'].mean()
+        results["user info"]["average age of users"] = round(average_age, 2)
+    
+    return results
 
 def display_raw_data(df):
     start_idx = 0
     while True:
-        raw_data_request = input("\nWould you like to see 5 lines of raw data? Enter 'yes' or 'no': ").lower()
+        raw_data_request = input("\nWould you like to see 5 more lines of raw data? Enter 'yes' or 'no': ").lower()
         if raw_data_request == 'yes':
             print(df.iloc[start_idx:start_idx + 5])
             start_idx += 5
@@ -86,14 +167,14 @@ def main():
             print("Invalid city. Please choose from Chicago, New York City, or Washington.\n")
 
     while True:
-        month = input("Enter the month (January, February ,march, april, may, june) or 'all' for all months: ").lower()
+        month = input("Enter the month (January, February, March, April, May, June) or 'all' for all months: ").lower()
         if month in ['all', 'january', 'february', 'march', 'april', 'may', 'june']:
             break
         else:
             print("Invalid month. Please enter a valid month or 'all' for all months.\n")
 
     while True:
-        day = input("Enter the day of the week (e.g., Monday, Tuesday ...) or 'all' for all days: ").lower()
+        day = input("Enter the day of the week (e.g., Monday, Tuesday, ...) or 'all' for all days: ").lower()
         if day in ['all', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
             break
         else:
@@ -104,8 +185,14 @@ def main():
 
     if df is not None:
         statistics = calculate_statistics(df)
-        for key, value in statistics.items():
-            print(f"{key}: {value}")
+        if statistics:  # Check if statistics is not empty
+            for group, group_stats in statistics.items():
+                print('\n')
+                print(group.upper())
+                for key, value in group_stats.items():
+                    print(f"{key}: {value}")
+        else:
+            print("No data available for the selected filters.")
 
         display_raw_data(df)
 
